4083723b0c45c6db7416bf50a473d160c640ba55
Bharath466 <32551151+Bharath466@users.noreply.github.com>
2021-04-03 12:05:03 +0530

    Add files via upload

---
+++ src/components/characters/Character.Grid.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../../App.css'
+import CharacterItem from './CharacterItem';
+import Spinners from '../ui/Spinners'
+
+const CharacterGrid=({items,isLoading})=>{
+
+   return isLoading ? (
+      <Spinners />
+   ):(
+       <section className='cards'>
+{items.map(item=>(
+    <CharacterItem key={item.char_id}  item={item}></CharacterItem>
+))}
+       </section>
+   )
+   
+}
+export default CharacterGrid;
---
+++ src/components/characters/Footers.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footers=()=>{
+    return(
+        <div className='Foo' >
+            <h4>----Designed by Bharath----- </h4>
+        </div>
+    )
+}
+export default Footers
